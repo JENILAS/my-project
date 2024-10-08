b5a9145d6eab92d83f60088581bf6cb4a1c22283
Jenila.Soosai <Jenila.Soosai@plains.com>
2024-10-08 21:44:43 +0530

    Star Rating

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Accordion from './components/accordion';
+import StarRating from './components/star-rating';
 
 function App() {
   return (
     <div className="App">
       <Accordion/>
+      <StarRating/>
     </div>
   );
 }
