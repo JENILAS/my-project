b27ff8b8dd7096744863897614320b6615820cd7
Jenila.Soosai <Jenila.Soosai@plains.com>
2024-10-12 22:07:30 +0530

    Weather App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Accordion from './components/accordion';
 import StarRating from './components/star-rating';
+import WeatherApp from './components/weather-app';
 
 function App() {
   return (
     <div className="App">
       <Accordion/>
       <StarRating/>
+      <WeatherApp/>
     </div>
   );
 }
